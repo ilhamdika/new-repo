6c73db007640018ee7d8ae26e12b6addf7f0fceb
ilhamdika <93665288+ilhamdika@users.noreply.github.com>
2024-02-10 15:52:05 +0700

    feat: cerate tabel view list

--- resources/js/Pages/Admin/Transaksi/ListData.jsx
+++ resources/js/Pages/Admin/Transaksi/ListData.jsx
@@ -1,9 +1,52 @@
 import LayoutAdmin from "@/Layouts/Admin/Index"
+import PrimaryButton from "@/Components/PrimaryButton"
+import { Link } from "@inertiajs/react";
+import { CiEdit } from "react-icons/ci";
+import { MdDeleteOutline } from "react-icons/md";
 
 export default function Index(){
     return(
         <LayoutAdmin>
-            <h1>List data</h1>
+            <h1 className="text-3xl mb-3">List data</h1>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Deskripsi</th>
+                        <th>Code</th>
+                        <th>Rate Euro</th>
+                        <th>Kategori</th>
+                        <th>Nama Transaksi</th>
+                        <th>Nominal</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>
+                            test
+                        </td>
+                        <td>Naruto</td>
+                        <td>Naruto</td>
+                        <td>Naruto</td>
+                        <td>Naruto</td>
+                        <td>Action</td>
+                        <td>
+                            <Link>
+                                <PrimaryButton className="bg-yellow-500">
+                                    <CiEdit size={20} />
+                                </PrimaryButton>
+                            </Link>
+                            <PrimaryButton className="bg-red-600">
+                                <MdDeleteOutline size={20} />
+                            </PrimaryButton>
+                        </td>
+                    </tr>
+                    
+                </tbody>
+            </table>
         </LayoutAdmin>
     )
 }
